f87d0ea74b3487f07c114a4ab49c35802f4c60fb
Konstantin <kkuchmasov@bk.ru>
2026-08-01 00:47:33 +0300

    second

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -15,41 +17,72 @@ const (
 type DB []Row
 
 type Row struct {
-	id   int
-	name string
+	ID   int
+	Name string
 }
 
 var db DB
 
 func main() {
-	for true {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
 		var comandName string
-		fmt.Print("Введите команду - ")
-		fmt.Scan(&comandName)
+		var args []string
+
+		determineCommand(scanner, &comandName, &args)
+
 		switch strings.ToLower(comandName) {
 		case INSERT:
-			insert()
+			insert(&args)
 		case SELECT:
 			selectAll()
 		case EXIT:
 			os.Exit(0)
+		default:
+			fmt.Println("Команда не известна " + comandName)
 		}
 	}
 }
 
-func insert() {
-	var newRow Row
-	lendb := len(db)
-	if lendb != 0 {
-		newRow.id = db[lendb-1].id + 1
+func determineCommand(scanner *bufio.Scanner, comandName *string, args *[]string) {
+	vScanner := *scanner
+	if !vScanner.Scan() {
+		return
 	}
-	fmt.Print("Введите name ")
-	fmt.Scan(&newRow.name)
-	db = append(db, newRow)
+
+	line := vScanner.Text()
+	stringSplit := strings.Split(line, " ")
+	if len(stringSplit) == 0 {
+		return
+	}
+
+	*comandName = stringSplit[0]
+	if len(stringSplit) > 1 {
+		*args = stringSplit[1:]
+	}
+}
+
+func insert(args *[]string) {
+	vArgs := *args
+	if len(vArgs) == 2 {
+		id, err := strconv.Atoi(vArgs[0])
+		if err != nil {
+			fmt.Println("id не равен числу")
+			return
+		}
+		newRow := Row{
+			ID:   id,
+			Name: vArgs[1],
+		}
+		db = append(db, newRow)
+	} else {
+		fmt.Println("Неверное число параметров")
+	}
+
 }
 
 func selectAll() {
 	for i := range db {
-		fmt.Printf("id %d; name %s;\n", db[i].id, db[i].name)
+		fmt.Printf("id %d; name %s;\n", db[i].ID, db[i].Name)
 	}
 }
