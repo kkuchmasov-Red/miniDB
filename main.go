65a65cfe01a3ff9293fb8816306b53a8ff45d85b
Konstantin <kkuchmasov@bk.ru>
2026-08-02 02:20:31 +0300

    test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,9 +10,13 @@ import (
 )
 
 const (
-	INSERT = "insert"
-	SELECT = "select"
-	EXIT   = "exit"
+	INSERT    = "insert"
+	SELECT    = "select"
+	EXIT      = "exit"
+	UPDATE    = "update"
+	DELETE    = "delete"
+	EXIT_FAST = "0"
+	TEST      = "test"
 )
 
 type DB []Row
@@ -21,22 +26,24 @@ type Row struct {
 	Name string
 }
 
-var db DB
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	var db DB
 	for {
-		var comandName string
-		var args []string
-
-		determineCommand(scanner, &comandName, &args)
+		comandName, args := parserCommand(scanner)
 
 		switch strings.ToLower(comandName) {
 		case INSERT:
-			insert(&args)
+			db.Insert(args)
+		case UPDATE:
+			db.Update(args)
+		case DELETE:
+			db.Delete(args)
 		case SELECT:
-			selectAll()
-		case EXIT:
+			db.SelectAll()
+		case TEST:
+			test()
+		case EXIT, EXIT_FAST:
 			os.Exit(0)
 		default:
 			fmt.Println("Команда не известна " + comandName)
@@ -44,45 +51,120 @@ func main() {
 	}
 }
 
-func determineCommand(scanner *bufio.Scanner, comandName *string, args *[]string) {
-	vScanner := *scanner
-	if !vScanner.Scan() {
-		return
-	}
+func test() {
 
-	line := vScanner.Text()
-	stringSplit := strings.Split(line, " ")
-	if len(stringSplit) == 0 {
-		return
-	}
-
-	*comandName = stringSplit[0]
-	if len(stringSplit) > 1 {
-		*args = stringSplit[1:]
-	}
 }
 
-func insert(args *[]string) {
-	vArgs := *args
-	if len(vArgs) == 2 {
-		id, err := strconv.Atoi(vArgs[0])
-		if err != nil {
-			fmt.Println("id не равен числу")
-			return
-		}
+func parserCommand(scanner *bufio.Scanner) (string, []string) {
+	var comandName string
+	var args []string
+	if !scanner.Scan() {
+		return comandName, args
+	}
+
+	line := scanner.Text()
+	stringSplit := strings.Fields(line)
+	if len(stringSplit) == 0 {
+		return comandName, args
+	}
+
+	comandName = stringSplit[0]
+	if len(stringSplit) > 1 {
+		args = stringSplit[1:]
+	}
+	return comandName, args
+}
+
+func CheckArgs(args []string, needCount int) error {
+	if len(args) != needCount {
+		return errors.New("Неверное количество аргументов")
+	}
+	return nil
+}
+
+func IDInArgs(args []string, position int) (int, error) {
+	return strconv.Atoi(args[position])
+}
+
+func (db *DB) Insert(args []string) {
+	err := CheckArgs(args, 2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	id, err := IDInArgs(args, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if db.Exists(id) {
+		fmt.Println("id уже существует")
+	} else {
 		newRow := Row{
 			ID:   id,
-			Name: vArgs[1],
+			Name: args[1],
 		}
-		db = append(db, newRow)
-	} else {
-		fmt.Println("Неверное число параметров")
+		*db = append(*db, newRow)
 	}
-
 }
 
-func selectAll() {
-	for i := range db {
-		fmt.Printf("id %d; name %s;\n", db[i].ID, db[i].Name)
+func (db *DB) Update(args []string) {
+	err := CheckArgs(args, 2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	id, err := IDInArgs(args, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := range *db {
+		if (*db)[i].ID == id {
+			(*db)[i].Name = args[1]
+			return
+		}
+	}
+	fmt.Println("id не существует")
+}
+
+func (db *DB) Delete(args []string) {
+	err := CheckArgs(args, 1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	id, err := IDInArgs(args, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := range *db {
+		if (*db)[i].ID == id {
+			*db = append((*db)[:i], (*db)[i+1:]...)
+			return
+		}
+	}
+
+	fmt.Println("id не существует")
+}
+func (db *DB) Exists(id int) bool {
+	for _, row := range *db {
+		if row.ID == id {
+			return true
+		}
+	}
+	return false
+}
+
+func (db *DB) SelectAll() {
+	for _, row := range *db {
+		fmt.Printf("id %d; name %s;\n", row.ID, row.Name)
 	}
 }
